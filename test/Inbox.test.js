da4ed337c9401b269ce4fd1bea1b93c53d5c429d
Fynn Breuer <breuerfynn@gmail.com>
2021-11-13 00:13:58 +0100

    web3 and ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,4 @@
 const assert = require("assert");
 const ganache = require("ganache-cli"); // local eth test network
-const Web3 = require("web3");
+const Web3 = require("web3"); // uppercase -> constructor
+const web3 = new Web3(ganache.provider()); // lowercase --> instance
