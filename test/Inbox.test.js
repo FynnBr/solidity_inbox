08fbacf61ec60bb0b29f33e8a0856d3783077d3a
Fynn Breuer <breuerfynn@gmail.com>
2021-11-13 21:41:42 +0100

    mocha tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,25 @@ const assert = require("assert");
 const ganache = require("ganache-cli"); // local eth test network
 const Web3 = require("web3"); // uppercase -> constructor
 const web3 = new Web3(ganache.provider()); // lowercase --> instance
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // get list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // use one of these accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
