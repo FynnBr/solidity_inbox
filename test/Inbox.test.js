a7c467831656fe81da34b6c105d94747894a3a9b
Fynn Breuer <breuerfynn@gmail.com>
2021-11-15 21:55:16 +0100

    + deploy.js added with infura api + double quotes to single quotes

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,8 @@
-const assert = require("assert");
-const ganache = require("ganache-cli"); // local eth test network
-const Web3 = require("web3"); // uppercase -> constructor
+const assert = require('assert');
+const ganache = require('ganache-cli'); // local eth test network
+const Web3 = require('web3'); // uppercase -> constructor
 const web3 = new Web3(ganache.provider()); // lowercase --> instance
-const { interface, bytecode } = require("../compile");
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
@@ -14,24 +14,24 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hi there!"],
+      arguments: ['Hi there!'],
     })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
-describe("Inbox", () => {
-  it("deploys a contract", () => {
+describe('Inbox', () => {
+  it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
 
-  it("has a default message", async () => {
+  it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, "Hi there!");
+    assert.equal(message, 'Hi there!');
   });
 
-  it("can change the message", async () => {
-    await inbox.methods.setMessage("Hello!").send({ from: accounts[0] });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('Hello!').send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
-    assert.equal(message, "Hello!");
+    assert.equal(message, 'Hello!');
   });
 });
